1691f379a1dff62b8ca850b40978873e527305e8
Cinling <1105412681@qq.com>
2021-06-04 11:42:58 +0800

    添加测试代码

---
+++ Lib/Utils/JsonUtilTest.cs
@@ -0,0 +1,15 @@
+﻿using Cinling.Lib.Utils;
+using NUnit.Framework;
+
+namespace Test.Lib.Utils {
+    
+    public class JsonUtilTest {
+
+        [Test]
+        public void encode() {
+            var obj = new {a = 1, b = "2"};
+            var objJson = JsonUtil.encode(obj);
+            Assert.IsTrue(objJson == "{\"a\":1,\"b\":\"2\"}");
+        }
+    }
+}
